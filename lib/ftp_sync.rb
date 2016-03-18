5555698a145a84a802318aec94deefde858dba69
Scott Bronson <brons_git@rinspin.com>
2016-03-18 15:30:22 -0700

    sync local mtime with remote
    this ensures clock skew between local and remote can't
    screw up the sync operation.

--- lib/ftp_sync.rb
+++ lib/ftp_sync.rb
@@ -54,7 +54,7 @@ class FtpSync
         recurse << paths
       elsif entry.file?
         if options[:since] == :src
-          tocopy << paths unless File.exist?(paths[0]) and entry.mtime < File.mtime(paths[0]) and entry.filesize == File.size(paths[0])
+          tocopy << [*paths, entry.mtime] unless File.exist?(paths[0]) and entry.mtime <= File.mtime(paths[0]) and entry.filesize == File.size(paths[0])
         elsif options[:since].is_a?(Time)
           tocopy << paths unless entry.mtime < options[:since] and File.exist?(paths[0]) and entry.filesize == File.size(paths[0])
         else
@@ -65,10 +65,11 @@ class FtpSync
     end
     
     tocopy.each do |paths|
-      localfile, remotefile = paths
+      localfile, remotefile, mtime = paths
       unless should_ignore?(localfile)
         begin
           @connection.get(remotefile, localfile)
+          FileUtils.touch(localfile, mtime: mtime) if mtime
           log "Pulled file #{remotefile}"
         rescue Net::FTPPermError
           log "ERROR READING #{remotefile}"
